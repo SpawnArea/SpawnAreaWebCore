0e8dc5f31e822caa3d2d97f6f00dc5d77b82eccd
Immanuel Van Meirhaeghe <immanuelvm@live.be>
2016-09-14 19:02:12 +0200

    Basic working sample demo base code MVC ASP .NET using JwPlayer as media player in HTML5.

---
+++ src/SpawnAreaWebCore/Controllers/MediaController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpawnAreaWebCore.Models;
+
+namespace SpawnAreaWebCore.Controllers
+{
+    public class MediaController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            try
+            {
+                return View(new MediaFile() { VideoId = "", SourceUrl = new Uri("https://www.youtube.com/") });
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult LoadPlayer(string videoid, Uri sourceurl)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return View("MediaPlayer", new MediaFile() { VideoId = videoid, SourceUrl = sourceurl });
+                }
+                return View("Index");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ src/SpawnAreaWebCore/Models/MediaFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpawnAreaWebCore.Models
+{
+    public class MediaFile
+    {
+        public string VideoId { get; set; }
+        [Url]
+        public Uri SourceUrl { get; set; }
+
+    }
+}
